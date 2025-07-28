1be3a084fd94605fa53a6cfa8db9bcb3ee21707b
carlhung <carl.hung@yahoo.com>
2025-07-29 00:51:39 +0100

    removed log_in vc. moved to home screen for the login logic.

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:ukopenrice/view_controllers/add_restaurant_information.dart';
-import 'package:ukopenrice/view_controllers/log_in.dart';
-// import 'package:ukopenrice/view_controllers/open_hours_selector.dart';
 import 'package:ukopenrice/view_controllers/remove_restaurant_info.dart';
 import 'package:ukopenrice/view_controllers/restaurant_input_mode.dart';
 import 'package:ukopenrice/view_controllers/upload_photos.dart';
@@ -11,21 +9,17 @@ class Routes {
   // Route name constants
   static const String uploadPhotoScreen = '/upload_restaurant_photos';
   static const String homeScreen = '/';
-  static const String logInScreen = '/login';
   static const String addRestaurantInfoScreen = '/add_restaurant_information';
   static const String restaurantInputMode = '/restaurant_input_mode';
   static const String removeRestaurantInfo = '/remove_restaurant_info';
-  // static const String openHoursSelector = '/open_hours_selector';
-  // static const String openHoursSelector = '/';
 
   /// The map used to define routes to be provided in [MaterialApp]
   static final Map<String, WidgetBuilder> allRoutes = {
-    homeScreen: (context) => const HomeScreen(),
-    logInScreen: (context) => const LogIn(),
+    homeScreen: (context) =>
+        const HomeScreen(), //const RestaurantHomePage(), //const HomeScreen(),
     addRestaurantInfoScreen: (context) => const AddResturantInformation(),
     uploadPhotoScreen: (context) => const UploadPhotos(),
     restaurantInputMode: (context) => const RestaurantInputMode(),
     removeRestaurantInfo: (context) => const RemoveRestaurantInfo(),
-    // openHoursSelector: (context) => const OpenHoursSelectorViewController(),
   };
 }
